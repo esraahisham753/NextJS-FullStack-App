d6b8d4f02b3d5134aff3df13e5568c554fb17b14
Esraa Abduallah <esraahisham753@gmail.com>
2024-05-19 15:56:57 +0000

    Add header styles and logo

--- events-app/src/components/header/header.jsx
+++ events-app/src/components/header/header.jsx
@@ -1,16 +1,27 @@
+import Image from "next/image";
 import Link from "next/link";
+import logo from "./background.jpg";
 
 const Header = () => (
   <header>
-    <nav>
-      <img />
-      <Link href="/">Home</Link>
-      <Link href="/about-us">About Us</Link>
-      <Link href="/events">Events</Link>
-    </nav>
+    <div className="topNav">
+      <Image src={logo} alt="logo" width={50} height={50} />
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about-us">About Us</Link>
+          </li>
+          <li>
+            <Link href="/events">Events</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <h1>Es-Events App</h1>
   </header>
 );
 
 export default Header;
-
-
