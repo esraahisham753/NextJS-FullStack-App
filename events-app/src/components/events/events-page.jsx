0ad12cfa6ae7bca0c53ac5f690771d329fe78f7e
Esraa Abduallah <esraahisham753@gmail.com>
2024-05-20 15:52:58 +0000

    Complete events page styling

--- events-app/src/components/events/events-page.jsx
+++ events-app/src/components/events/events-page.jsx
@@ -1,20 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const EventsMain = ({data}) => {
+const EventsMain = ({ data }) => {
   return (
-    <div>
-      <h1>Events page</h1>
-      <div className="events_page">
-        {data.map((ev) => {
-          return (
-            <Link key={ev.id} href={`/events/${ev.id}`}>
-              <Image src={ev.image} alt={ev.title} width={300} height={300} />
-              <h2>{ev.title}</h2>
-            </Link>
-          );
-        })}
-      </div>
+    <div className="events_page">
+      {data.map((ev) => {
+        return (
+          <Link className="card" key={ev.id} href={`/events/${ev.id}`}>
+            <Image src={ev.image} alt={ev.title} width={300} height={400} />
+            <h2>{ev.title}</h2>
+          </Link>
+        );
+      })}
     </div>
   );
 };
